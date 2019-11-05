034b4bf15ec26b1d9a7f16359462e40316fcc9d8
Sam Fearn <sam.fearn@gmail.com>
2019-11-05 12:32:39 +0000

    switched to single space layout to avoid accidental spaces from layer tap

--- keyboards/planck/keymaps/samfearn/keymap.c
+++ keyboards/planck/keymaps/samfearn/keymap.c
@@ -58,14 +58,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |SCSft |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  Up  |SCSft |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | FUNCT| Ctrl | Alt  | CMD  |   SPLOWER   |  SPRAISE    |CMD/At| Left | Down |Right |
+ * | FUNCT| Ctrl | Alt  | CMD  |Lower |    Space    | Raise|CMD/At| Left | Down |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     TD(TD_QWERTESC),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     HYPR_T(KC_TAB),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_ENT,
     KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP, KC_RSPC ,
-    FUNCT, TD(TD_ALTSFT), KC_LALT, KC_LCMD, SPLOWER,   SPLOWER,  SPRAISE,  SPRAISE,   TD(TD_CMDALT), KC_LEFT, KC_DOWN,   KC_RGHT
+    FUNCT, TD(TD_ALTSFT), KC_LALT, KC_LCMD, LOWER, KC_SPC, KC_SPC, RAISE,   TD(TD_CMDALT), KC_LEFT, KC_DOWN,   KC_RGHT
 ),
 
 /* Lower
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | SCLSB|      |      |      |      |      |      |  1P  |  ;   |   \  | Vol+ | SCRSB|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |             |             | Play | Prev | Vol- | Next |
+ * |      |      |      |      |      |             |      | Play | Prev | Vol- | Next |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
@@ -95,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | SCLCB|      |      |      |      |      |      |  ?   |   :  |  /   | Vol+ | SCRCB|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |             |             | Play | Prev | Vol- | Next |
+ * |      |      |      |      |      |             |      | Play | Prev | Vol- | Next |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
@@ -113,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |  <   |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|      |      |      |      |   >  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |             |             |      |      |      |      |
+ * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_planck_grid(
@@ -131,14 +131,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 * |------+------+------+------+------+------|------+------+------+------+------+------|
 * |      |  F9  |  F10 |  F11 |  F12 |  1   |  2   |  3   |  ,   |  .   |  MSU | Click|
 * |------+------+------+------+------+------+------+------+------+------+------+------|
-* |      |      |      |      |      0      |     Ent     |  =   |  MSL |  MSD |  MSR |
+* |      |      |      |      |      |      0      |  Ent |  =   |  MSL |  MSD |  MSR |
 * `-----------------------------------------------------------------------------------'
 */
 [_FUNCT] = LAYOUT_planck_grid(
    _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_P7, KC_P8, KC_P9, KC_PAST,  KC_PSLS, KC_MS_WH_UP, KC_DEL ,
    _______, KC_F5, KC_F6, KC_F7, KC_F8, KC_P4, KC_P5, KC_P6, KC_PPLS,  KC_PMNS, KC_MS_WH_DOWN, KC_PENT ,
    _______, KC_F9, KC_F10, KC_F11, KC_F12, KC_P1, KC_P2, KC_P3, KC_PCMM,  KC_PDOT, KC_MS_U, KC_MS_BTN1 ,
-   _______, _______, _______, _______, KC_P0, KC_P0, KC_PENT, KC_PENT, KC_PEQL,  KC_MS_L, KC_MS_D, KC_MS_R
+   _______, _______, _______, _______, _______, KC_P0, KC_P0, KC_PENT, KC_PEQL,  KC_MS_L, KC_MS_D, KC_MS_R
 ),
 
 /* TRUEQWERT (TapDancing Toggle)
@@ -149,7 +149,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 * |------+------+------+------+------+------|------+------+------+------+------+------|
 * |      |      |      |      |      |      |      |      |      |      |      |      |
 * |------+------+------+------+------+------+------+------+------+------+------+------|
-* |      |      |      |      |             |             |      |      |      |      |
+* |      |      |      |      |      |             |      |      |      |      |      |
 * `-----------------------------------------------------------------------------------'
 */
 [_TRUEQWERT] = LAYOUT_planck_grid(
